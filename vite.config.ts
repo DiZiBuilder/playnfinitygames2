ad936e66c94e8d09bfb3dd70b816c3d9811aad98
DiZiBuilder <dkzotkov@gmail.com>
2026-01-22 11:12:34 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from "path";
