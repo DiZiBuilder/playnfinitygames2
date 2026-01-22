2a72561c77f837716a414afa7f079943717ca8e5
DiZiBuilder <dkzotkov@gmail.com>
2026-01-22 11:02:31 +0200

    Resolve conflicts and finalize upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -19,3 +19,26 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+=======
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: '/playnfinitygames2/',  
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
+
