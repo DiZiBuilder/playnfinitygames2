d1d232b7bfa995011e705005c9794672a85be289
DiZiBuilder <dkzotkov@gmail.com>
2026-01-24 11:24:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/playnfinitygames2/',
+  base: '/',  // <-- FIXED for Netlify root deploy
   server: {
     host: "::",
     port: 8080,
